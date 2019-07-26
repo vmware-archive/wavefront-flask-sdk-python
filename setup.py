133eee443a4238b40a94d2aa71dfb5805a5dc9ea
Hao Song <songhao@vmware.com>
2019-07-26 10:30:55 -0700

    Remove using six

--- setup.py
+++ setup.py
@@ -1,33 +1,47 @@
+# !/usr/bin/env python3
 # coding: utf-8
+"""Wavefront Flask SDK.
 
+<p>This is a Wavefront Flask SDK</p>
 """
-    Wavefront Flask SDK
-    <p>This is a Wavefront Flask SDK</p>  # noqa: E501
-"""
 
-from setuptools import setup, find_packages  # noqa: H301
+import os
 
-NAME = 'wavefront_flask_sdk'
-VERSION = '0.1.0'
-# To install the library, run the following
-#
-# python setup.py install
-#
-# prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
+import setuptools
 
-REQUIRES = ['opentracing>=2.0,<3', 'six', 'flask', 'wavefront-pyformance', 'Werkzeug']
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                       'README.md')) as fd:
+    LONG_DESCRIPTION = fd.read()
 
-setup(
-    name=NAME,
-    version=VERSION,
-    description="Wavefront Flask Python SDK",
-    author_email="songhao@vmware.com",
-    url="https://github.com/wavefrontHQ/wavefront-flask-sdk-python",
-    keywords=["Wavefront SDK", "Wavefront", "Flask"],
-    install_requires=REQUIRES,
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+setuptools.setup(
+    name='wavefront-flask-sdk-python',
+    version='0.1.0',
+    author='Wavefront by VMware',
+    author_email='songhao@vmware.com',
+    url='https://github.com/wavefrontHQ/wavefront-flask-sdk-python',
+    license='Apache-2.0',
+    description='Wavefront Flask SDK',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    keywords=[
+        'Wavefront',
+        'Wavefront SDK',
+        'Flask'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
     include_package_data=True,
-    long_description="""\
-    """
+    packages=setuptools.find_packages(exclude=('*.tests', '*.tests.*',
+                                               'tests.*', 'tests')),
+    install_requires=(
+        'opentracing>=2.0',
+        'flask>=1.0',
+        'Werkzeug>=0.14',
+        'wavefront-opentracing-sdk-python>=1.2',
+    )
 )
